20dbad289e938a2151292da6086c73a6cd092de3
skog03 <aumale.annija@gmail.com>
2025-05-24 16:03:35 +0300

    finished

---
+++ cypress/e2e/FinalTask.cy.js
@@ -0,0 +1,49 @@
+import { HomePage } from "../pageObjects.js/HomePage";
+
+describe('template spec', () => {
+  beforeEach(() => {
+    HomePage.visit();
+  });
+
+  it('', () => {
+    // b. Input all the necessary information in the text fields, buttons, etc, where it is not
+    // stated otherwise.
+    HomePage.firstNameField.type("John");
+    HomePage.lastNameField.type("Doe");
+    HomePage.emailField.type("johndoe@gmail.com");
+    HomePage.maleChk.check({force: true});
+    HomePage.phoneNumber.type("1234567891");
+    // c. Set the - Date of Birth - with Calendar widget to - 28th of February, 1930.
+    HomePage.DOB.click();
+    HomePage.yearSelect.select("1930");
+    HomePage.monthSelect.select("1");
+    HomePage.daySelect.click();
+    // d. Set Subjects to Economics.
+    HomePage.subjects.type("eco{enter}");
+    // e. Set Hobbies to Music.
+    HomePage.musicChk.click({force: true});
+    // f. Upload an image of your choice.
+    HomePage.fileInput.selectFile('cypress/files/Gegy hous.jpg');
+    // i. Create folder “files” and put the image there
+    // ii. Might come in handy: https://docs.cypress.io/api/commands/selectfile
+    
+    //address
+    HomePage.addressField.type("Hollywood street 1");
+    // g. Set State to NCR.
+    HomePage.stateSelect.type("NCR{enter}");
+    // h. Set City to Delhi.
+    HomePage.citySelect.type("Delhi{enter}");
+    // i. Click Submit.
+    HomePage.submitBtn.click({force: true});
+    // j. Validate that each Labeled row contains the correct information.
+    HomePage.endTable.should('contain.text','John Doe');
+    HomePage.endTable.should('contain.text','johndoe@gmail.com');
+    HomePage.endTable.should('contain.text','1234567891');
+    HomePage.endTable.should('contain.text','28 February,1930');
+    HomePage.endTable.should('contain.text','Economics');
+    HomePage.endTable.should('contain.text','Music');
+    HomePage.endTable.should('contain.text','Gegy hous.jpg');
+    HomePage.endTable.should('contain.text','Hollywood street 1');
+    HomePage.endTable.should('contain.text','NCR Delhi');
+  })
+})
---
+++ cypress/pageObjects.js/HomePage.js
@@ -0,0 +1,78 @@
+import { BasePage } from "./BasePage";
+
+
+export class HomePage extends BasePage {
+  static get url() {
+    return "https://demoqa.com/automation-practice-form";
+  }
+
+  static get firstNameField() {
+    return cy.get("#firstName");
+  }
+
+  static get lastNameField() {
+    return cy.get("#lastName");
+  }
+
+  static get emailField() {
+    return cy.get("#userEmail");
+  }
+
+  static get maleChk() {
+    return cy.get("#gender-radio-1");
+  }
+  
+  static get phoneNumber() {
+    return cy.get("#userNumber");
+  }
+  static get DOB() {
+    return cy.get("#dateOfBirthInput");
+  }
+  
+  static get yearSelect() {
+    return cy.get("[class='react-datepicker__year-select']");
+  }
+  
+  static get monthSelect() {
+    return cy.get("[class='react-datepicker__month-select']");
+  }
+  
+  static get daySelect() {
+    return cy.get("[aria-label='Choose Friday, February 28th, 1930']");
+  }
+  
+  static get subjects() {
+    return cy.get("#subjectsContainer");
+  }
+
+  static get musicChk() {
+    return cy.get("#hobbies-checkbox-3");
+  }
+
+  static get fileInput() {
+    return cy.get("#uploadPicture");
+  }
+  
+  static get addressField() {
+    return cy.get("#currentAddress");
+  }
+  
+  static get stateSelect() {
+    return cy.get("#state");
+  }
+
+  static get citySelect() {
+    return cy.get("#city");
+  }
+
+  static get submitBtn() {
+    return cy.get("#submit");
+  }
+
+
+  //----------------------------------------------------------------
+
+  static get endTable(){
+    return cy.get("[class='table-responsive']");
+  }
+}
